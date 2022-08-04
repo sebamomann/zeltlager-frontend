faae423e9317ef420bf1834e40dc382e5f35b6cb
sebamomann <github@sebamomann.de>
2022-08-04 20:36:43 +0200

    fix base url

--- src/assets/env.template.js
+++ src/assets/env.template.js
@@ -2,5 +2,5 @@
   window["env"] = window["env"] || {};
   window["env"]["production"] = true;
   window["env"]["API_URL"] = "${API_URL}";
-  window["env"]["BASE_URL"] = "{BASE_URL}";
+  window["env"]["BASE_URL"] = "${BASE_URL}";
 })(this);
