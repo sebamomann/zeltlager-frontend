bb1e2c9a2caaa9ae6f3c4a9bcdd6120c58159321
sebamomann <github@sebamomann.de>
2022-08-06 11:37:34 +0200

    visualize parent with tree too

--- src/app/tree/tree-list/tree-list.component.ts
+++ src/app/tree/tree-list/tree-list.component.ts
@@ -1,3 +1,4 @@
+import { element } from 'protractor';
 import { NestedTreeControl } from '@angular/cdk/tree';
 import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 import { MatTreeNestedDataSource } from '@angular/material';
@@ -9,20 +10,51 @@ import { MatTreeNestedDataSource } from '@angular/material';
 })
 export class TreeListComponent implements OnInit {
 
-
+  @Input() treeNode: string;
   @Input() elements: any;
 
   ngOnChanges(changes: SimpleChanges) {
-    this.dataSource.data = changes.elements.currentValue;
+    this.manipulate();
   }
 
-  treeControl = new NestedTreeControl<any>(node => node.children);
+  treeControl = undefined;
   dataSource = new MatTreeNestedDataSource<any>();
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
-  hasChild = (_: number, node: any) => !!node.children && node.children.length > 0;
+  ngOnInit() {
+    this.manipulate();
+  }
+
+  public manipulate() {
+    if (!Array.isArray(this.elements)) {
+      this.elements = [this.elements];
+      this.elements = this.prepareElements(this.elements);
+    }
+
+    this.treeControl = new NestedTreeControl<any>(node => node[this.treeNode]);
+    this.dataSource.data = this.elements
+  }
+
+  public prepareElements(elements) {
+    console.log(JSON.stringify(elements))
+
+    if (!elements[0]) {
+      return [];
+    }
+
+    if (!elements[0][this.treeNode]) {
+      elements[0][this.treeNode] = []
+    } else {
+      const ret = this.prepareElements([elements[0][this.treeNode]]);
+      if (ret.length > 0) {
+        elements[0][this.treeNode] = ret;
+      }
+    }
+
+    return elements;
+  }
+
+  hasChild = (_: number, node: any) => !!node[this.treeNode] && node[this.treeNode].length > 0;
 }
